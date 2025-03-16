898c9bf3e25e0ef1dd0819589663a47a4bd290ff
dmingn <dmingn@users.noreply.github.com>
2025-03-16 12:57:20 +0900

    add tbs channel

--- app/main.py
+++ app/main.py
@@ -3,12 +3,13 @@ from xml.etree.ElementTree import tostring
 from fastapi import FastAPI, Response
 
 from app.channel import Channel
-from app.channels import nhk_e1_130, nhk_g1_130, ntv
+from app.channels import nhk_e1_130, nhk_g1_130, ntv, tbs
 
 path_to_channel: dict[str, Channel] = {
     "joak-dtv": nhk_g1_130,
     "joab-dtv": nhk_e1_130,
     "joax-dtv": ntv,
+    "jorx-dtv": tbs,
 }
 
 
