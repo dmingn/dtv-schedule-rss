0bd1221bb838bf9f6e0bf976240e2d657e683c3a
dmingn <dmingn@users.noreply.github.com>
2025-03-20 10:04:43 +0900

    add mx-tv channel

--- app/channels/__init__.py
+++ app/channels/__init__.py
@@ -1,4 +1,5 @@
 from .fujitv import fujitv
+from .mx_tv import mx_tv_1, mx_tv_2
 from .nhk import nhk_e1_130, nhk_g1_130
 from .ntv import ntv
 from .tbs import tbs
--- app/main.py
+++ app/main.py
@@ -3,7 +3,17 @@ from xml.etree.ElementTree import tostring
 from fastapi import FastAPI, Response
 
 from app.channel import Channel
-from app.channels import fujitv, nhk_e1_130, nhk_g1_130, ntv, tbs, tv_asahi, tv_tokyo
+from app.channels import (
+    fujitv,
+    mx_tv_1,
+    mx_tv_2,
+    nhk_e1_130,
+    nhk_g1_130,
+    ntv,
+    tbs,
+    tv_asahi,
+    tv_tokyo,
+)
 
 path_to_channel: dict[str, Channel] = {
     "joak-dtv": nhk_g1_130,
@@ -13,6 +23,8 @@ path_to_channel: dict[str, Channel] = {
     "jocx-dtv": fujitv,
     "joex-dtv": tv_asahi,
     "jotx-dtv": tv_tokyo,
+    "jomx-dtv-1": mx_tv_1,
+    "jomx-dtv-2": mx_tv_2,
 }
 
 
